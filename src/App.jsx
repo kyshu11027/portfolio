a5113f6f8a72d7a2cba8d3684442ab5aec8edcbe
Personal <shuster5494@gmail.com>
2023-11-18 16:07:44 -0500

    starting about me body

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import AboutMe from "./components/AboutMe";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
@@ -10,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <AboutMe />
     </>
   );
 }
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./about-me.css";
+
+const AboutMe = () => {
+  return (
+    <div className="about-me-container">
+      <div className="about-me">About me text</div>
+
+      <div className="hobbies">Hobbies</div>
+    </div>
+  );
+};
+
+export default AboutMe;

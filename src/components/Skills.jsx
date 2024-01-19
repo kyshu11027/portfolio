f2ee34d7bcfeb2d1f99bb40437680e443e07294b
Personal <shuster5494@gmail.com>
2024-01-19 14:43:58 -0500

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "../css/skills.css";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="skills-container container-fluid">
+      <div className="row justify-content-center">
+        <div className="skills-card col-12">
+          <div className="languages">
+            <h3> Languages</h3>
+            <ul>
+              <li>Python</li>
+              <li>Javascript</li>
+              <li>Java</li>
+              <li>C++</li>
+              <li>Go</li>
+              <li>HTML/CSS</li>
+              <li>Swift</li>
+              <li>MATLAB</li>
+            </ul>
+          </div>
+          <div className="technologies">
+            <h3> Technologies</h3>
+            <ul>
+              <li>Git</li>
+              <li>Jupyter Notebook</li>
+              <li>Sci-kit Learn</li>
+              <li>Kubernetes</li>
+              <li>Docker</li>
+              <li>Linux</li>
+              <li>Bash</li>
+              <li>Bootstrap</li>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>React</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;

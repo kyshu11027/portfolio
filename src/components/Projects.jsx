9d6c3e7cb7bdc3c0b41dc12c82eec4052e54eec8
Personal <shuster5494@gmail.com>
2025-01-20 15:18:24 -0600

    added pickpix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,37 +3,51 @@ import Project from "./Project.jsx";
 import ootd from "/assets/ootd.png";
 import peakRec from "/assets/peak-rec.png";
 import website from "/assets/website.png";
+import pickpix from "/assets/pickpix.png";
 import "../css/projects.css";
 
 const Projects = () => {
-  return (
-    <div className="project-container">
-      <Project
-        name="OOTD App"
-        link="https://github.com/kyshu11027/OOTD"
-        description="iOS App for fashion fanatics who want to build their virtual closet and log their favorite outfits."
-        tech={["Storyboard", "Swift", "Firebase", "Figma"]}
-        img={ootd}
-        className="row"
-      />
-      <Project
-        name="Peak Recognizer"
-        link="https://github.com/kyshu11027/PeakIdentifier/"
-        description="Data science project to recognize target peaks in microchip electrophoresis traces. "
-        tech={["Python", "Jupyter", "Sci-kit Learn"]}
-        img={peakRec}
-        className="row"
-      />
-      <Project
-        name="Personal Portfolio"
-        link="https://github.com/kyshu11027/portfolio"
-        description="My personal online portfolio that tells the world a bit about myself, lists my favorite projects, and shows my contact information."
-        tech={["Javascript", "React", "Bootstrap"]}
-        img={website}
-        className="row"
-      />
-    </div>
-  );
+    return (
+        <div className="project-container">
+            <Project
+                name="PickPix"
+                link="https://pickpix.vercel.app/"
+                description="Browser application for social media users to share photos with their friends to determine the most post-worthy content."
+                tech={[
+                    "AWS (Lambdas, DynamoDB, S3)",
+                    "Python",
+                    "NextJS",
+                    "TailwindCSS",
+                ]}
+                img={pickpix}
+                className="row"
+            />
+            <Project
+                name="OOTD App"
+                link="https://github.com/kyshu11027/OOTD"
+                description="iOS App for fashion fanatics who want to build their virtual closet and log their favorite outfits."
+                tech={["Storyboard", "Swift", "Firebase", "Figma"]}
+                img={ootd}
+                className="row"
+            />
+            <Project
+                name="Peak Recognizer"
+                link="https://github.com/kyshu11027/PeakIdentifier/"
+                description="Data science project to recognize target peaks in microchip electrophoresis traces. "
+                tech={["Python", "Jupyter", "Sci-kit Learn"]}
+                img={peakRec}
+                className="row"
+            />
+            <Project
+                name="Personal Portfolio"
+                link="https://github.com/kyshu11027/portfolio"
+                description="My personal online portfolio that tells the world a bit about myself, lists my favorite projects, and shows my contact information."
+                tech={["Javascript", "React", "Bootstrap"]}
+                img={website}
+                className="row"
+            />
+        </div>
+    );
 };
 
 export default Projects;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,43 +2,43 @@ import React from "react";
 import "../css/skills.css";
 
 const Skills = () => {
-  return (
-    <div className="skills-container container-fluid">
-      <div className="row justify-content-center">
-        <div className="skills-card col-12">
-          <div className="languages">
-            <h3> Languages</h3>
-            <ul>
-              <li>Python</li>
-              <li>Javascript</li>
-              <li>Java</li>
-              <li>C++</li>
-              <li>Go</li>
-              <li>HTML/CSS</li>
-              <li>Swift</li>
-              <li>MATLAB</li>
-            </ul>
-          </div>
-          <div className="technologies">
-            <h3> Technologies</h3>
-            <ul>
-              <li>Git</li>
-              <li>Jupyter Notebook</li>
-              <li>Sci-kit Learn</li>
-              <li>Kubernetes</li>
-              <li>Docker</li>
-              <li>Linux</li>
-              <li>Bash</li>
-              <li>Bootstrap</li>
-              <li>Node.js</li>
-              <li>Express.js</li>
-              <li>React</li>
-            </ul>
-          </div>
+    return (
+        <div className="skills-container container-fluid">
+            <div className="row justify-content-center">
+                <div className="skills-card col-12">
+                    <div className="languages">
+                        <h3> Languages</h3>
+                        <ul>
+                            <li>Python</li>
+                            <li>Javascript/Typescript</li>
+                            <li>Java</li>
+                            <li>C++</li>
+                            <li>Go</li>
+                            <li>HTML/CSS</li>
+                            <li>Swift</li>
+                            <li>MATLAB</li>
+                        </ul>
+                    </div>
+                    <div className="technologies">
+                        <h3> Technologies</h3>
+                        <ul>
+                            <li>Git</li>
+                            <li>Jupyter Notebook</li>
+                            <li>Sci-kit Learn</li>
+                            <li>Docker</li>
+                            <li>AWS</li>
+                            <li>Linux</li>
+                            <li>Bash</li>
+                            <li>Bootstrap</li>
+                            <li>Node.js</li>
+                            <li>Express.js</li>
+                            <li>React</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Skills;
